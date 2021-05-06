8fcfd8fe471b0f0beaaa7182b05f8b48a4ecdee1
mggons <malagonsxp@hotmail.com>
2021-05-06 09:14:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const fs = require('fs')
 const moment = require('moment-timezone')
 const { exec } = require('child_process')
 const fetch = require('node-fetch')
-const tiktod = require('tiktok-scraper')
 const ffmpeg = require('fluent-ffmpeg')
 const wmusica = JSON.parse(fs.readFileSync('./src/wmusica.json')) //añadida entrada para grupo de musica
 const result = JSON.parse(fs.readFileSync('./src/result.json'))

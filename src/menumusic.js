477f54a342f3836dcb14d45440bac72d1dbcf110
mggons <malagonsxp@hotmail.com>
2021-07-09 10:02:11 -0500

    Add files via upload

---
+++ src/menumusic.js
@@ -0,0 +1,49 @@
+const menumusic = (prefix) => {
+return ` 
+┏━━━━❉ *Menu del FullTono* ❉━━━━━┓
+┣⊱ *Hola y Bienvenidos a Fulltono*
+┣⊱ *Si eres Android puedes usar los*
+┣⊱ *comandos con normalidad pero si*
+┣⊱ *Eres de IOS solo debes usar los*
+┣⊱ *comandos: #ytbuscar,#mp4,#dlmp4*
+┣⊱ =====================
+┣⊱ *Comandos de Grupo Musica Administrador*
+┣⊱ =====================
+┣⊱ *${prefix}setprefix* -> Cambia el codigo inicial del grupo
+┣⊱ *${prefix}tagall* -> En lista a todos con etiqueta 
+┣⊱ *${prefix}cambios* -> Permite ver los cambios del BOT
+┣⊱ *${prefix}cambionm* -> Cambia el nombre del grupo 
+┣⊱ *${prefix}cambiodc* -> Cambia descripcion del grupo
+┣⊱ *${prefix}fotogrupo* -> Cambia foto del grupo
+┣⊱ *${prefix}musireglas* -> Reglas del grupo fulltono
+┣⊱ *${prefix}menumusica* -> Menu del grupo de musica
+┣⊱ =====================
+┣⊱ *Comandos de Busqueda*
+┣⊱ =====================
+┣⊱ *${prefix}ytbuscar* -> Busca en youtube 
+┣⊱ =====================
+┣⊱ *Comandos de Descarga Android*
+┣⊱ =====================
+┣⊱ *${prefix}mp3* -> Sacar audio de Youtube
+┣⊱ *${prefix}mp4* -> Sacar video de Youtube
+┣⊱ *${prefix}dlmp3* -> Sacar audio de Youtube
+┣⊱ *${prefix}dlmp4* -> Sacar video de Youtube
+┣⊱ *${prefix}bkmp3* -> Sacar audio de Youtube
+┣⊱ *${prefix}bkmp4* -> Sacar video de Youtube
+┣⊱ *${prefix}joox* -> Sacar link de Joox Music
+┣⊱ *${prefix}descarga* -> Sacar audio de Joox Music
+┣⊱ *${prefix}soundcloud* -> Bajar pista de Soundcloud
+┣⊱ =====================
+┣⊱ *Comandos de Descarga Iphone*
+┣⊱ =====================
+┣⊱ *${prefix}mp4* -> Sacar video de Youtube
+┣⊱ *${prefix}dlmp4* -> Sacar video de Youtube
+┣⊱ *${prefix}bkmp4* -> Sacar video de Youtube
+┣⊱ =====================
+┣⊱ *Texto a Voz*
+┣⊱ =====================
+┣⊱ *${prefix}tts* -> Permite pasar de texto a Voz
+┣⊱ =====================
+┗━━━━━━━━━━━━━━━┛`
+}
+exports.menumusic = menumusic
---
+++ src/musirules.js
@@ -0,0 +1,26 @@
+const musirules = (prefix) => {
+return `
+┏━━━━━❉ _*Acerca del BOT*_ ❉━━━━┓
+┣⊱  *Nombre*  : 
+┣⊱ *Grupo WP* : 
+┣⊱ *Grupo TG* : 
+┣⊱ *Web Page* : 
+┣⊱ *Creador*  : 
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+┏━━━━❉ *Reglas del Grupo* ❉━━━━━┓
+┣⊱ 1.
+┣⊱ 2.
+┣⊱ 3.
+┣⊱ 4. Aqui se añaden las reglas
+┣⊱ 5.
+┣⊱ 6.
+┣⊱ 7.
+┣⊱ 8.
+┣⊱ 9.
+┣⊱ 10.
+┣⊱ 11.
+┣⊱ 12.
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛`
+}
+exports.musirules = musirules

418c34557d38cc97925556a489dc648bd0e03ba3
mggons <malagonsxp@hotmail.com>
2021-07-09 09:58:43 -0500

    Update install.sh
    cambios menores

--- install.sh
+++ install.sh
@@ -5,9 +5,6 @@ apt-get upgrade
 apt-get install nodejs -y
 apt-get install libwebp -y
 apt-get install ffmpeg -y
-apt-get install wget -y
-apt-get install tesseract -y
-wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
 
 echo "[*] Se han instalado todas las dependencias, ejecute el comando \"npm start\" para iniciar inmediatamente el script"

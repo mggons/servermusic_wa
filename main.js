279b033f1a7d072fe37f50863fd1efe2c665b4b5
mggons <malagonsxp@hotmail.com>
2022-05-13 19:21:29 -0500

    Update main.js

--- main.js
+++ main.js
@@ -210,3 +210,6 @@ function uncache(module = '.') {
 }
 
 starts()
+const port = process.env.PORT || 3000;
+const host = '0.0.0.0'
+.listen(port, host, ()=> connsole.log(`server is running on port ${port}`)
